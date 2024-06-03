4ef956ccb221208f30e7886079c866c95e5746f8
Ian Beal <i.beal@outlook.com>
2024-06-02 16:43:53 -1000

    clean up a few more things

--- src/lib/tag.svelte
+++ src/lib/tag.svelte
@@ -1,16 +1,25 @@
 <script>
-    /**
+	/**
 	 * @type {string}
 	 */
-    export let text;
-    let colors=[
-      1,2,3
-    ];
-    // if (document.getElementById('topBody')?.classList.contains('dark')) {
-    // }
-    let i = Math.floor(Math.random()*3);
+	export let text;
+  const themeColorEnum = {
+    primary: 0,
+    secondary: 1,
+    tertiary: 2
+  }
+  /**
+   * number between 1-3 indicating which theme color to use
+   * @returns themeColorEnum
+   */
+	let themeColorIndex = Math.floor(Math.random() * 3);
 </script>
 
-<div class="inline-block rounded-3xl px-2 py-1 m-1 min-w-16" class:bg-primary={i == 0} class:bg-secondary={i == 1} class:bg-tertiary={i == 2}>
-    <p class="text-center mx-1 text-gray-dark">{ text }</p>
+<div
+	class="inline-block rounded-3xl px-2 py-1 m-1 min-w-16"
+	class:bg-primary={themeColorIndex == themeColorEnum.primary}
+	class:bg-secondary={themeColorIndex == themeColorEnum.secondary}
+	class:bg-tertiary={themeColorIndex == themeColorEnum.tertiary}
+>
+	<p class="text-center mx-1 text-gray-dark">{text}</p>
 </div>
